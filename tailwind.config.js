6ffa86e7eb9225276ea47f5ec1b5c5ef3fc0756c
Vonrazel <bonyrazelmorales@gmail.com>
2025-08-23 01:55:11 +0800

    Update project files and resolve merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,3 +11,17 @@ export default {
   plugins: [],
 }
 
+=======
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+>>>>>>> b652a15d6e125003c3ec9529481d383a2f7762cf
